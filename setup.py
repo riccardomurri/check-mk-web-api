30a9baf9a04ac08b043457d8deae76024c8f4a64
Riccardo Murri <riccardo.murri@gmail.com>
2020-03-31 16:24:44 +0200

    Change author and contact email address.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='check_mk_web_api',
-    packages=['check_mk_web_api'],
+    name='cmkclient',
+    packages=['cmkclient'],
     version='1.6',
-    description='Library to talk to Check_Mk Web API',
-    author='Max Brenner',
-    author_email='xamrennerb@gmail.com',
-    url='https://github.com/brennerm/check-mk-web-api',
-    download_url='https://github.com/brennerm/check-mk-web-api/archive/1.6.tar.gz',
+    description='A library and command-line client to talk to Check_Mk Web API',
+    author='Max Brenner, Riccardo Murri',
+    author_email='riccardo.murri@gmail.com',
+    url='https://github.com/riccardomurri/cmkclient',
+    download_url='https://github.com/riccardomurri/cmkclient/archive/1.4.tar.gz',
     install_requires=[
         'enum34;python_version<"3.4"',
         'fire',
